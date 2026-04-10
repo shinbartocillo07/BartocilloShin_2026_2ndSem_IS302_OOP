529d3e799e0dd1e8ddcb4f2004f80fca8e64ba88
shinbartocillo07 <shinbartocillo435@gmail.com>
2026-04-10 11:41:10 +0800

    Rename second number variable and update calculations

--- 2nd_Semester/IS302/04_10_26/oop-python-course/week01_intro/calculator.py
+++ 2nd_Semester/IS302/04_10_26/oop-python-course/week01_intro/calculator.py
@@ -1,11 +1,11 @@
 num1_SGB = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num2_SGB = float(input("Enter second number: "))
 
-print("Addition:", num1_SGB + num2)
-print("Subtraction:", num1_SGB - num2)
-print("Multiplication:", num1_SGB * num2)
+print("Addition:", num1_SGB + num2_SGB)
+print("Subtraction:", num1_SGB - num2_SGB)
+print("Multiplication:", num1_SGB * num2_SGB)
 
-if num2 != 0:
-    print("Division:", num1_SGB / num2)
+if num2_SGB != 0:
+    print("Division:", num1_SGB / num2_SGB)
 else:
     print("Division: Cannot divide by zero")
